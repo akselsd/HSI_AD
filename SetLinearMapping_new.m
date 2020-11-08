3d67d1c14d93919707b6b9a9f2b88ca6a3c3efd5
akselgundersen <aksel.gundersen@hotmail.com>
2020-11-08 18:49:34 +0100

    deleted files

--- SetLinearMapping_new.m
+++
@@ -1,7 +0,0 @@
-function DBN = SetLinearMapping_new( DBN, V, OUT )
-    nrbm = numel(DBN.rbm);
-    
-    Hall            = v2hall_new( DBN, V );
-    DBN.rbm{nrbm}.W = pinv(Hall{nrbm-1}) * OUT;
-    DBN.rbm{nrbm}.b = -0.5 * ones(size(DBN.rbm{nrbm}.b));
-end
